563897a88515b87e5bb8b0b75b348e92a76f93b3
leneandersen <lene@kvalitetsit.dk>
2018-10-12 14:15:56 +0200

    remove custom securiry annotation

--- medcom-video-api-service/src/main/java/dk/medcom/video/api/controller/SchedulingInfoController.java
+++ medcom-video-api-service/src/main/java/dk/medcom/video/api/controller/SchedulingInfoController.java
@@ -72,7 +72,7 @@ public class SchedulingInfoController {
 		return resource;
 	}
 
-	@APISecurityAnnotation({UserRole.PROVISIONER, UserRole.PROVISIONER_USER})
+//	@APISecurityAnnotation({UserRole.PROVISIONER, UserRole.PROVISIONER_USER})
 	@RequestMapping(value = "/scheduling-info/{uuid}", method = RequestMethod.PUT)
 	public Resource <SchedulingInfoDto> updateSchedulingInfo(@PathVariable("uuid") String uuid, @Valid @RequestBody UpdateSchedulingInfoDto updateSchedulingInfoDto ) throws RessourceNotFoundException, PermissionDeniedException, NotValidDataException {
 		LOGGER.debug("Entry of /scheduling-info.put uuid: " + uuid);
