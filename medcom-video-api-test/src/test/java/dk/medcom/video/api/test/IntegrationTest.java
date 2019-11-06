7e95cc3932a1803a3c08a90d22b2935fc6098a81
Jonas Pedersen <jpe@kvalitetsit.dk>
2019-11-06 08:59:28 +0100

    Run postman/newman scripts as part of integration test. For now only simple alive test is running.

--- medcom-video-api-test/src/test/java/dk/medcom/video/api/test/IntegrationTest.java
+++ medcom-video-api-test/src/test/java/dk/medcom/video/api/test/IntegrationTest.java
@@ -1,56 +1,111 @@
 package dk.medcom.video.api.test;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.rules.TemporaryFolder;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.matchers.Times;
 import org.mockserver.model.Header;
+import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.*;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
-@RunWith(TestRunner.class)
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
 public class IntegrationTest {
+	private static final Logger mysqlLogger = LoggerFactory.getLogger("mysql");
+	private static final Logger videoApiLogger = LoggerFactory.getLogger("video-api");
+	private static final Logger mockServerLogger = LoggerFactory.getLogger("mock-server");
+	private static final Logger newmanLogger = LoggerFactory.getLogger("newman");
 
-/*	@BeforeClass
-	public static void testIntegrationTest() throws UnsupportedOperationException, IOException, InterruptedException {
-		
-		// Given
-		Network n = Network.newNetwork();
+	private static boolean commandLine;
 
-		MySQLContainer mysql = (MySQLContainer) new MySQLContainer("mysql:5.5").withDatabaseName("videodb").withUsername("videouser").withPassword("secret1234").withNetwork(n).withNetworkAliases("mysql");
+	private static Network dockerNetwork;
+	private static GenericContainer resourceContainer;
+
+	@BeforeClass
+	public static void setup() {
+		dockerNetwork = Network.newNetwork();
+
+        resourceContainer = new GenericContainer<>(new ImageFromDockerfile()
+                .withFileFromClasspath("/collections/medcom-video-api.postman_collection.json", "docker/collections/medcom-video-api.postman_collection.json")
+                .withFileFromClasspath("/loop.sh", "loop.sh")
+                .withDockerfileFromBuilder( builder -> builder.from("bash")
+                        .add("/collections/medcom-video-api.postman_collection.json", "/collections/medcom-video-api.postman_collection.json")
+                        .add("/loop.sh", "/loop.sh")
+                        .volume("/collections")
+                        .volume("/testresult")
+                        .cmd("sh", "/loop.sh")
+                        .build()));
+
+        resourceContainer.start();
+        System.out.println("Created: " + resourceContainer.isCreated());
+
+		// SQL server for Video API.
+		MySQLContainer mysql = (MySQLContainer) new MySQLContainer("mysql:5.5")
+				.withDatabaseName("videodb")
+				.withUsername("videouser")
+				.withPassword("secret1234")
+				.withNetwork(dockerNetwork)
+				.withNetworkAliases("mysql");
 		mysql.start();
+		attachLogger(mysql, mysqlLogger);
 
+		// Mock server
+		MockServerContainer userService = new MockServerContainer()
+				.withNetwork(dockerNetwork)
+				.withNetworkAliases("userservice");
+		userService.start();
+		attachLogger(userService, mockServerLogger);
+		MockServerClient mockServerClient = new MockServerClient(userService.getContainerIpAddress(), userService.getMappedPort(1080));
+		mockServerClient.when(HttpRequest.request().withMethod("GET"), Times.unlimited()).respond(getResponse());
 
-		
-		GenericContainer us = new GenericContainer<>("kuk/luk:latest")
-		.withNetwork(n)
-		.withNetworkAliases("userservice");
-		us.start();
-
-		
+		// VideoAPI
 		GenericContainer videoApi = new GenericContainer<>("kvalitetsit/medcom-video-api-web:latest")
-				.withNetwork(n)
+				.withNetwork(dockerNetwork)
 				.withNetworkAliases("videoapi")
 				.withEnv("CONTEXT", "/api")
 				.withEnv("jdbc_url", "jdbc:mysql://mysql:3306/videodb")
 				.withEnv("jdbc_user", "videouser")
 				.withEnv("jdbc_pass", "secret1234")
-				.withEnv("userservice_url", "http://userservice:9200")
+				.withEnv("userservice_url", "http://userservice")
 				.withEnv("userservice_token_attribute_organisation", "organisation_id")
 				.withEnv("userservice_token_attribute_username", "username")
 				.withEnv("userservice.token.attribute.email", "email")
 				.withEnv("userservice.token.attribute.userrole", "userrole")
 				.withEnv("scheduling.template.default.conferencing.sys.id", "22")
-				.withEnv("scheduling.template.default.uri.prefix", "abc") 
-				.withEnv("scheduling.template.default.uri.domain", "test.dk") 
-				.withEnv("scheduling.template.default.host.pin.required", "true") 
-				.withEnv("scheduling.template.default.host.pin.range.low", "1000") 
-				.withEnv("scheduling.template.default.host.pin.range.high", "9999") 
-				.withEnv("scheduling.template.default.guest.pin.required", "true") 
-				.withEnv("scheduling.template.default.guest.pin.range.low", "1000") 
-				.withEnv("scheduling.template.default.guest.pin.range.high", "9999") 
-				.withEnv("scheduling.template.default.vmravailable.before", "15") 
-				.withEnv("scheduling.template.default.max.participants", "10") 
-				.withEnv("scheduling.template.default.end.meeting.on.end.time", "true") 
-				.withEnv("scheduling.template.default.uri.number.range.low", "1000") 
-				.withEnv("scheduling.template.default.uri.number.range.high", "9999") 
+				.withEnv("scheduling.template.default.uri.prefix", "abc")
+				.withEnv("scheduling.template.default.uri.domain", "test.dk")
+				.withEnv("scheduling.template.default.host.pin.required", "true")
+				.withEnv("scheduling.template.default.host.pin.range.low", "1000")
+				.withEnv("scheduling.template.default.host.pin.range.high", "9999")
+				.withEnv("scheduling.template.default.guest.pin.required", "true")
+				.withEnv("scheduling.template.default.guest.pin.range.low", "1000")
+				.withEnv("scheduling.template.default.guest.pin.range.high", "9999")
+				.withEnv("scheduling.template.default.vmravailable.before", "15")
+				.withEnv("scheduling.template.default.max.participants", "10")
+				.withEnv("scheduling.template.default.end.meeting.on.end.time", "true")
+				.withEnv("scheduling.template.default.uri.number.range.low", "1000")
+				.withEnv("scheduling.template.default.uri.number.range.high", "9999")
 				.withEnv("scheduling.template.default.ivr.theme", "10")
 				.withEnv("scheduling.info.citizen.portal", "https://portal.vconf.dk")
 				.withEnv("mapping.role.provisioner", "dk:medcom:role:provisioner")
@@ -60,18 +115,65 @@ public class IntegrationTest {
 				.withExposedPorts(8080)
 				.waitingFor(Wait.forHttp("/api/info").forStatusCode(200));
 		videoApi.start();
+		attachLogger(videoApi, videoApiLogger);
 
-		
+
 	}
-*/
+
+	private static void attachLogger(GenericContainer container, Logger logger) {
+		logger.info("Attaching logger to container: " + container.getContainerInfo().getName());
+		Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
+		container.followOutput(logConsumer);
+	}
+
 	@Test
-	public void test() throws InterruptedException {
-		
-	}
-	
-	private static HttpResponse getResponse() {  
+	public void verifyTestResults() throws InterruptedException, IOException, ParserConfigurationException, SAXException, XPathExpressionException {
+		Thread.sleep(5000);
+		TemporaryFolder folder = new TemporaryFolder();
+		folder.create();
 
-		return new HttpResponse().withBody("{\"organisation_id\":\"klak\",\"email\":\"eva@klak.dk\"}").withHeaders(new Header("Content-Type", "application/json")).withStatusCode(200);
+		GenericContainer newman = new GenericContainer<>("postman/newman_ubuntu1404:4.1.0")
+					.withNetwork(dockerNetwork)
+					.withVolumesFrom(resourceContainer, BindMode.READ_WRITE)
+					.withCommand("run /collections/medcom-video-api.postman_collection.json -r cli,junit --reporter-junit-export /testresult/TEST-dk.medcom.video.api.test.IntegrationTest.xml --global-var host=videoapi --global-var port=8080");
+
+		newman.start();
+		attachLogger(newman, newmanLogger);
+
+		long waitTime = 500;
+		long loopLimit = 60;
+
+		for(int i = 0; newman.isRunning() && i < loopLimit; i++) {
+			System.out.println(i);
+			System.out.println("Waiting....");
+			Thread.sleep(waitTime);
+		}
+
+		resourceContainer.copyFileFromContainer("/testresult/TEST-dk.medcom.video.api.test.IntegrationTest.xml", folder.getRoot().getCanonicalPath() + "/TEST-dk.medcom.video.api.test.IntegrationTest.xml");
+
+		FileInputStream input = new FileInputStream(folder.getRoot().getCanonicalPath() + "/TEST-dk.medcom.video.api.test.IntegrationTest.xml");
+		DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = builderFactory.newDocumentBuilder();
+		Document xmlDocument = builder.parse(input);
+		XPath xPath = XPathFactory.newInstance().newXPath();
+		String failureExpression = "/testsuites/testsuite/@failures";
+		String errorExpression = "/testsuites/testsuite/@errrors";
+
+		int failures = ((Double) xPath.compile(failureExpression).evaluate(xmlDocument, XPathConstants.NUMBER)).intValue();
+		int errors = ((Double) xPath.compile(errorExpression).evaluate(xmlDocument, XPathConstants.NUMBER)).intValue();
+
+		if(errors != 0 || failures != 0) {
+			StringBuilder stringBuilder = new StringBuilder();
+			Files.readAllLines(Paths.get(folder.getRoot().getCanonicalPath() + "/TEST-dk.medcom.video.api.test.IntegrationTest.xml")).forEach(x -> stringBuilder.append(x).append(System.lineSeparator()));
+			System.out.println(stringBuilder);
+		}
+
+		assertEquals(0, failures);
+		assertEquals(0, errors);
+	}
+
+	private static HttpResponse getResponse() {
+		return new HttpResponse().withBody("{\"UserAttributes\": {\"organisation_id\": [\"pool-test-org\"],\"email\":[\"eva@klak.dk\"],\"userrole\":[\"ADMIN\"]}}").withHeaders(new Header("Content-Type", "application/json")).withStatusCode(200);
 	}
 }
 
