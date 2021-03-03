73114abc1d87839dad05c6abc59a364418dddf78
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-03-03 08:57:16 +0100

    Unit test findOrganisationByGroupId.

--- medcom-video-api-service/src/test/java/dk/medcom/vdx/organisation/dao/OrganisationDaoTest.java
+++ medcom-video-api-service/src/test/java/dk/medcom/vdx/organisation/dao/OrganisationDaoTest.java
@@ -4,6 +4,7 @@ package dk.medcom.vdx.organisation.dao;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
 
@@ -49,5 +50,37 @@ public class OrganisationDaoTest extends RepositoryTest {
     public void testOrganisationNotFound() {
         var result = organisationDao.findOrganisation("not_found");
         assertNull(result);
+    }
+
+    @Test
+    public void testQueryOrganisationWithoutPoolByGroupId() {
+        var result = organisationDao.findOrganisationByGroupId(13L);
+
+        assertNotNull(result);
+        assertEquals(12L, result.getParentId().longValue());
+        assertEquals(13L, result.getGroupId().longValue());
+        assertEquals("child org", result.getOrganisationName());
+        assertEquals("child", result.getOrganisationId());
+        assertEquals("child", result.getGroupName());
+        assertNull(result.getPoolSize());
+    }
+
+    @Test
+    public void testQueryOrganisationWithPoolByGroupId() {
+        var result = organisationDao.findOrganisationByGroupId(11L);
+
+        assertNotNull(result);
+        assertEquals(10L, result.getParentId().longValue());
+        assertEquals(11L, result.getGroupId().longValue());
+        assertEquals(20, result.getPoolSize().intValue());
+        assertEquals("parent org", result.getOrganisationName());
+        assertEquals("parent", result.getOrganisationId());
+        assertEquals("parent", result.getGroupName());
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void testOrganisationByGroupIdNotFound() {
+        long notFound = 47382;
+        organisationDao.findOrganisationByGroupId(notFound);
     }
 }
