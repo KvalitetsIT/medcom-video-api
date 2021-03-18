83158f875e38e9b2cbb23b0ba10a22bc27ffb4ea
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-03-18 13:23:03 +0100

    Remove debug log of headers.

--- medcom-video-api-service/src/main/java/dk/medcom/video/api/interceptor/LoggingInterceptor.java
+++ medcom-video-api-service/src/main/java/dk/medcom/video/api/interceptor/LoggingInterceptor.java
@@ -1,8 +1,5 @@
 package dk.medcom.video.api.interceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -10,11 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import java.util.Spliterator;
-import java.util.Spliterators;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 public class LoggingInterceptor extends HandlerInterceptorAdapter {
 	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingInterceptor.class);
@@ -37,15 +32,7 @@ public class LoggingInterceptor extends HandlerInterceptorAdapter {
 		MDC.put(MDC_CORRELATION_ID, correlationId);
 
 		String requestUrl = request.getRequestURL().toString();
-		if (requestUrl != null) {
-			MDC.put(MDC_REQUEST_URL, requestUrl);
-		}
-
-		if(LOGGER.isDebugEnabled()) {
-			LOGGER.debug("HTTP headers: " + StreamSupport.stream(Spliterators.spliteratorUnknownSize(request.getHeaderNames().asIterator(), Spliterator.ORDERED), false)
-					.map(x -> String.format("%s=%s", x, request.getHeader(x)))
-					.collect(Collectors.joining(",")));
-		}
+		MDC.put(MDC_REQUEST_URL, requestUrl);
 
 		return true;
 	}
