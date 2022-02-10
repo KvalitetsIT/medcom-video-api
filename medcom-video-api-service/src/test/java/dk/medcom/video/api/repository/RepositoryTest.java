ca86d9c416b74afb89a7ab0e792129e23c2d3acf
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-02-10 08:12:27 +0100

    Upgrade MySQL driver.

--- medcom-video-api-service/src/test/java/dk/medcom/video/api/repository/RepositoryTest.java
+++ medcom-video-api-service/src/test/java/dk/medcom/video/api/repository/RepositoryTest.java
@@ -14,6 +14,8 @@ import org.testcontainers.containers.MySQLContainer;
 import dk.medcom.video.api.configuration.DatabaseConfiguration;
 import dk.medcom.video.api.configuration.TestConfiguration;
 
+import java.util.TimeZone;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @PropertySource("test.properties")
 @ContextConfiguration(
@@ -21,12 +23,15 @@ import dk.medcom.video.api.configuration.TestConfiguration;
   loader = AnnotationConfigContextLoader.class)
 @Transactional
 abstract public class RepositoryTest {
+	static {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC")); // Needed when upgrading from Connector/j 5 to 8 to avoid fucking up date/times.
+	}
 	@BeforeClass
 	public static void setupMySqlJdbcUrl() {
 		MySQLContainer mysql = (MySQLContainer) new MySQLContainer("mysql:5.7").withDatabaseName("videodb").withUsername("videouser").withPassword("secret1234");
 		mysql.start();
 				
-		String jdbcUrl = mysql.getJdbcUrl() + "?useSSL=false";
+		String jdbcUrl = mysql.getJdbcUrl() + "?useSSL=false&serverTimeZone=UTC";
 		System.setProperty("jdbc.url", jdbcUrl);
 	}
 }
