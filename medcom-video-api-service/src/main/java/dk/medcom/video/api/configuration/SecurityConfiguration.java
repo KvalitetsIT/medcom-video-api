dd65de0ef4708c321dec24d701a0c4803ea00d12
Amalie Dobel Christiansen <adc@kvalitetsit.dk>
2025-04-08 11:14:29 +0200

    Add security filter for v2

---
+++ medcom-video-api-service/src/main/java/dk/medcom/video/api/configuration/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package dk.medcom.video.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .securityMatcher(request -> request.getRequestURI().startsWith("/v2/"))
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().authenticated() // Validates jwt token before input validation and scope validation
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
